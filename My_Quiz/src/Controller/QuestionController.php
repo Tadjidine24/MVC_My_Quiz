641211c11cb08c7d536031991e4751fbc42efb24
Tamou Tadjidine <tadjidine.tamou@epitech.eu>
2020-04-27 03:24:50 +0200

    crud commmencer create fini

--- My_Quiz/src/Controller/QuestionController.php
+++ My_Quiz/src/Controller/QuestionController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Question;
 // use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 
 class QuestionController extends AbstractController
 {
@@ -29,4 +33,51 @@ class QuestionController extends AbstractController
             'posts' => $posts
         ]);
     }
+
+/**
+*@Route("/question/save")
+*/
+    public function save()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $question = new Question();
+        $question->setQuestion('Question');
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response('question enregisté avec id'.$question->getId());
+    }
+
+    /**
+    *@Route("/",name="question_list")
+    */
+    public function home()
+    {
+        //récupérer tous les question de la table question de la BD
+        //et les mettre dans le tableau $question
+        $question = $this->getDoctrine()->getRepository(Question::class)->findAll();
+        return $this->render('quiz/MesQuestions.html.twig',['question'=>$question]);
+    }
+/**
+*@Route("/quiz/new", name="new_question")
+*Method({"GET", "POST"})
+*/
+    public function new(Request $request)
+    {
+        $question = new Question();
+        $form = $this->createFormBuilder($question)
+        ->add('question',TypeTextType::class)
+        ->add('save',SubmitType::class, array('label' => 'Créer'))
+        ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $question = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($question);
+            $entityManager->flush();
+            return $this->redirectToRoute('question_list');
+        }
+        return $this->render('quiz/new.html.twig',['form' => $form->createView()]);
+    }
 }
+
